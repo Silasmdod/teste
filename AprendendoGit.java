fb2f3b7853ad85fc14afebf5f533b25486707808
Silas Matheus <silasmdod@gmail.com>
2021-05-24 13:26:18 -0300

    Commit teste aprendendo a usar GIt

---
+++ AprendendoGit.java
@@ -0,0 +1,17 @@
+package Aplicattion;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AprendendoGit {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		//teste
+		
+		
+	}
+
+	
+}
